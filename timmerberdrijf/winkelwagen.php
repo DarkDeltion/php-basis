fe4b34c5ecc316c85a857268f3c4b64d48cdf950
damien <161700122+DarkDeltion@users.noreply.github.com>
2024-11-05 12:09:04 +0100

    timmerbedrijf opdracht klaar

--- timmerberdrijf/winkelwagen.php
+++ timmerberdrijf/winkelwagen.php
@@ -5,9 +5,84 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>winkelwagen</title>
 </head>
-<body>
-    
+<body>    
     <?php
+    $totaal = 0;
+
+    $berken = $grenen = $hardhout = "";
+
+    $rechthoekig = $ronde = "";
+
+    $breedte = $lengte = $diameter = "";
+
+    $berkenerr = $grenenerr = $hardhouterr = $rechthoekigerr = $rondeerr = "";
+
+    $berkenprijs = 9.50;
+    $grenenprijs = 8.50;
+    $hardhoutprijs = 11.50;
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+     
+        if(isset($_POST["Berken"])){
+            if(isset($_POST["rechthoekig"])){
+                $breedte = $_POST["breedte"] / 1000;
+                $lengte = $_POST["lengte"] / 1000;
+                $opervlakte = $lengte * $breedte;
+                $totaal = $opervlakte * $berkenprijs;
+
+                echo "$opervlakte vierkante meter <br>";
+                echo "$totaal euro <br>";
+            }
+            if(isset($_POST["ronde"])){
+                $diameter = $_POST['Diameter'] / 1000;
+                $oppervlakte = pi() * $diameter;
+                $totaal = $oppervlakte * $berkenprijs;
+                
+                echo "$oppervlakte vierkante meter <br>";
+                echo "$totaal euro <br>";
+            }
+        }
+
+        if(isset($_POST["grenen"])){
+            if(isset($_POST["rechthoekig"])){
+                $breedte = $_POST["breedte"] / 1000;
+                $lengte = $_POST["lengte"] / 1000;
+                $opervlakte = $lengte * $breedte;
+                $totaal = $opervlakte * $grenenprijs;
+
+                echo "$opervlakte vierkante meter <br>";
+                echo "$totaal euro <br>";
+            }
+            if(isset($_POST["ronde"])){
+                $diameter = $_POST['Diameter'] / 1000;
+                $oppervlakte = pi() * $diameter;
+                $totaal = $oppervlakte * $grenenprijs;
+                
+                echo "$oppervlakte vierkante meter <br>";
+                echo "$totaal euro <br>";
+            }
+        }
+
+        if(isset($_POST["hardhout"])){
+            if(isset($_POST["rechthoekig"])){
+                $breedte = $_POST["breedte"] / 1000;
+                $lengte = $_POST["lengte"] / 1000;
+                $opervlakte = $lengte * $breedte;
+                $totaal = $opervlakte * $hardhoutprijs;
+
+                echo "$opervlakte vierkante meter <br>";
+                echo "$totaal euro <br>";
+            }
+            if(isset($_POST["ronde"])){
+                $diameter = $_POST['Diameter'] / 1000;
+                $oppervlakte = pi() * $diameter;
+                $totaal = $oppervlakte * $hardhoutprijs;
+                
+                echo "$oppervlakte vierkante meter <br>";
+                echo "$totaal euro <br>";
+            }
+        }
+    }   
 
     ?>
 </body>
