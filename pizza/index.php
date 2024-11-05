31ceac83c3d614e5282cfb41102ca481f25199d6
damien <161700122+DarkDeltion@users.noreply.github.com>
2024-11-05 12:18:59 +0100

    foto

---
+++ pizza/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pizza</title>
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>about hout</h1>
+
+    <form action="./winkelwagen.php" method="post">
+        
+        <img src="./img/spianata_piccante.jpg" alt=""><br>
+        <input type="checkbox" id="Berken" name="Berken">
+        <label for="Berken"> Pizza Spianata Piccante € 12,50</label> <br> <br>
+
+
+        <img src="./img/seppi.jpg" alt=""><br>
+        <input type="checkbox" id="grenen" name="grenen">
+        <label for="grenen">Pizza Seppi € 11,50</label> <br> <br>
+        
+        
+        <img src="./img/tirato.jpg" alt=""><br>
+        <input type="checkbox" id="hardhout" name="hardhout">
+        <label for="hardhout">Pizza Tirato € 10,50</label> <br> <br>
+        
+        <br><br>
+        
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>
+</body>
+</html>
